568cdb7a15e4b083b2757498a7697983882d2fe0
Salil Upadhyay <upsalil.01@gmail.com>
2023-07-18 01:07:17 +0530

    completed order updating

--- client/src/app/components/adminorder/adminorder.component.ts
+++ client/src/app/components/adminorder/adminorder.component.ts
@@ -1,51 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { adminOrder } from 'src/app/constants/models';
+import { AdminorderService } from 'src/app/services/adminorder.service';
 
 @Component({
   selector: 'app-adminorder',
   templateUrl: './adminorder.component.html',
   styleUrls: ['./adminorder.component.css'],
 })
-export class AdminorderComponent {
-  orderItems: adminOrder[] = [
-    {
-      id: 1,
-      name: 'Pizza',
-      price: 100,
-      status: 'Received',
-      rating: 4,
-    },
-    {
-      id: 2,
-      name: 'Burger',
-      price: 100,
-      status: 'Received',
-      rating: 4,
-    },
-    {
-      id: 3,
-      name: 'Pepsi',
-      price: 100,
-      status: 'Received',
-      rating: 4,
-    },
-    {
-      id: 4,
-      name: 'Kola',
-      price: 100,
-      status: 'Received',
-      rating: 4,
-    },
-    {
-      id: 5,
-      name: 'Fries',
-      price: 100,
-      status: 'Received',
-      rating: 4,
-    },
-  ];
+export class AdminorderComponent implements OnInit {
+  orderItems = <any>[];
+  constructor(
+    private orderService: AdminorderService,
+    private toast: ToastrService
+  ) {}
+
+  fetchOrders(): void {
+    this.orderService.fetchData().subscribe({
+      next: (res: any) => {
+        console.log(res);
+        this.orderItems = res.orders;
+      },
+      error: (error) => {
+        console.log(error);
+        this.toast.error('<p>Server Error</p>', '', {
+          enableHtml: true,
+        });
+      },
+    });
+  }
+  // fetching dishes on mount
+  ngOnInit(): void {
+    this.fetchOrders();
+  }
+
   updateOrderStatus(order: adminOrder) {
     console.log(order);
+    this.orderService.updateOrder(order).subscribe({
+      next: (res) => {
+        console.log(res);
+        this.fetchOrders();
+        this.toast.success('<p>Order Updated Successfully</p>', '', {
+          enableHtml: true,
+        });
+      },
+      error: (error) => {
+        console.log(error);
+        this.toast.error('<p>Server Error</p>', '', {
+          enableHtml: true,
+        });
+      },
+    });
     // this.orderService.updateOrderStatus(order.id, order.status).subscribe(() => {
     //   // Order status updated successfully
     //   // You can update the orders array or refresh the data from the server
--- client/src/app/services/adminorder.service.ts
+++ client/src/app/services/adminorder.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { adminOrder } from '../constants/models';
+import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AdminorderService {
-
-  constructor() { }
+  constructor(private authService: AuthService, private http: HttpClient) {}
+  fetchData() {
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${this.authService?.token}`
+    );
+    return this.http.get<any>(`${environment.apiUrl}/orders`, { headers });
+  }
+  updateOrder(order: adminOrder) {
+    const status = order.status;
+    const headers = new HttpHeaders().set(
+      'Authorization',
+      `Bearer ${this.authService?.token}`
+    );
+    return this.http.patch<any>(
+      `${environment.apiUrl}/orders/${order?.id}`,
+      { status },
+      { headers }
+    );
+  }
 }
